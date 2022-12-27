d31fcccf2b7fa1a9b348a1683bbe7712a8841e77
Ivan Skiridomov <ivan.skiridomov@gmail.com>
2022-12-27 20:21:02 +0200

    rename mod

--- register.go
+++ register.go
@@ -2,7 +2,7 @@
 package remotewrite
 
 import (
-	"github.com/grafana/xk6-output-prometheus-remote/pkg/remotewrite"
+	"github.com/boskiv/xk6-output-prometheus-remote/pkg/remotewrite"
 	"go.k6.io/k6/output"
 )
 
